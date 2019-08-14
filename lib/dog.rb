a5895351e36fab2fb28933d06d8e8d7f6f9f4188
Raul Sposito <raulsposito@gmail.com>
2019-08-14 14:22:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,31 +24,3 @@ class Dog
   end
 
 end
-
-# Add your code here
-#class Dog 
-#  
-#  @@all = []
-#  
-#  attr_accessor :name 
-#  
-#  def initialize(name)
-#    @name = name 
-#    @@all << self 
-#  end
-#  
-#  def self.all 
-#    @@all
-#  end
-#  
-#  def self.print_all
-#    @@all.each do |dog|
-#      puts dog.name 
-#    end
-#  end
-#  
-#  def save
-#    @@all << self 
-#  end
-#  
-#end#
