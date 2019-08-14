577cad6fa970712118c370b6052552f9dfa29aec
Raul Sposito <raulsposito@gmail.com>
2019-08-14 13:49:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,11 +10,11 @@ class Dog
     @@all << self 
   end
   
-  def all 
+  def self.all 
     @@all
   end
   
-  def print_all
+  def self.print_all
     @@all.each do |name|
       puts name
     end
